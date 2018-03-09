d7f9a09459197440f2b389bb54663a54069274ef
Sasha Patsel <sashapatsel@calvisitor-10-105-131-99.calvisitor.1918.berkeley.edu>
2018-03-09 12:26:04 -0800

    sending server and html folder to master

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 var db = require("./models");
 
 // Serve static content for the app from the "public" directory in the application directory.
-//app.use(express.static("public"));
+app.use(express.static("public"));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,10 +17,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Set Handlebars.
-// var exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars");
 
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 //require("./routes/auth-routes.js")(app);
