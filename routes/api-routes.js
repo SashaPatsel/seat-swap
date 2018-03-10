a833db2c6ee001f16febc68941be79942516cf3f
Sasha Patsel <sashapatsel@calvisitor-10-105-131-99.calvisitor.1918.berkeley.edu>
2018-03-09 16:06:58 -0800

    stage for api-routes pull

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,40 +1,80 @@
+
 var db = require("../models");
 
 module.exports = function(app) {
 
     //create a new user
     app.post("/users", function(req, res) {
-
+        db.User.create(req.body)
+        .then(function(dbUser) {
+            res.json(dbUser);
+        });
     });
 
     //return an user's record
     app.get("/api/users/:id", function(req, res) {
-
+        db.User.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbUser) {
+            res.json(dbUser);
+        })
     });
 
     //update an user's record
     app.put("/users/:id", function(req, res) {
-
+        db.User.update(
+            req.body,
+            {
+                where: {
+                    id: req.body.id
+                }
+            }).then(function(dbUser) {
+                res.json(dbUser);
+            });
     });
 
-    //add a subscription to an user
+    //add a subscription to a user
     app.post("/subscriptions", function(req, res) {
-
+        db.Subscription.create(req.body).then(function(dbSubscription) {
+          res.json(dbSubscription);
+        });
     });
 
     //return list of subscriptions for a given user
     app.get("/api/users/:id/subscriptions", function(req, res) {
-
+        db.Subscription.findAll({
+          where: {
+            userId: req.params.id
+          }
+        }).then(function(dbSubscription) {
+          res.json(dbSubscription);
+        });
     });
 
     //return a subscription
     app.get("/api/subscriptions/:id", function(req, res) {
-
+        db.Subscription.findOne({
+          where: {
+            id: req.params.id
+          }
+        }).then(function(dbSubscription) {
+          res.json(dbSubscription);
+        });
     });
 
     //update a subscription record
-    app.put("/subscriptions/:id", function(req, res) {
-
+    app.put("/api/subscriptions/:id", function(req, res) {
+        db.Subscription.update(
+          req.body,
+          {
+            where: {
+              id: req.body.id
+            }
+          }).then(function(dbSubscription) {
+          res.json(dbSubscription);
+        });
     });
 
     //add a ticket to a subscription
@@ -89,22 +129,44 @@ module.exports = function(app) {
 
     //add a watcher
     app.post("/watchers", function(req, res) {
-
+        db.Watcher.create(req.body).then(function(dbWatcher) {
+          res.json(dbWatcher);
+        });
     });
 
     //return a list of watchers for a specific user
     app.get("/api/users/:id/watchers", function(req, res) {
-
+        db.Watcher.findAll({
+          where: {
+            userId: req.params.id
+          }
+        }).then(function(dbWatcher) {
+          res.json(dbWatcher);
+        });
     });
 
     //update a watcher
     app.put("/watchers/:id", function(req, res) {
-
+        db.Watcher.update(
+          req.body,
+          {
+            where: {
+              id: req.body.id
+            }
+          }).then(function(dbWatcher) {
+          res.json(dbWatcher);
+        });
     });
 
     //remove a watcher
     app.delete("/watchers/:id", function(req, res) {
-
+        db.Watcher.destroy({
+          where: {
+            id: req.params.id
+          }
+        }).then(function(dbWatcher) {
+          res.json(dbWatcher);
+        });
     });
 
     //add a trade journal entry
