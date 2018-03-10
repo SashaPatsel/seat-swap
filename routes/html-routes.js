f579f875867446ff66b5f1530f6020ce5cc066e5
Joyce <joycelamsaungan@gmail.com>
2018-03-09 22:32:02 -0800

    adding routes for passport auth

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -12,6 +12,27 @@ module.exports = function(app) {
 			res.render("home", object);
 	});
 
+	app.get('/account', ensureAuthenticated, function(req, res) {
+        // console.log(req);
+        console.log('abc');
+        res.send('hello');
+    });
+
 };
 
+function ensureAuthenticated(req, res, next) {
+    console.log('check something', req.session.passport.user);
+    var userId;
+    if (req.isAuthenticated()) {
+        userId = req.session.passport.user;
+
+        console.log("request is authenticated");
+        return next();
+    } else {
+        userId = false;
+        console.log('request is not authenticated');
+        res.redirect('/');
+        return userId
+    }
+}
 
