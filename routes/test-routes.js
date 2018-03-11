ad323d5b113024677c4642c2ad60cc5cd0665b20
zrowe <paul.louis.rowe@gmailcom>
2018-03-11 03:11:57 -0700

    updating api routes and adding test route

---
+++ routes/test-routes.js
@@ -0,0 +1,81 @@
+// This is a test fixture to load dummy records into the models
+// if a model is empty.  Once we have permanent data, this will
+// not be necessary.
+
+var db = require("../models");
+
+module.exports = function(app) {
+
+    //just hit this api.  no parms required.
+    app.get("/api/loaddata", function(req, res) {
+        checkUser();
+        checkOrganization();
+        checkSubscription();
+        checkTicket();
+        res.end("OK", 200);
+    });
+
+    function checkUser() {
+        db.User.count().then(function(c) {
+            console.log("There are " + c + " users!")
+            if (c == 0) {
+                db.User.create({
+                    firstName: "Dummy",
+                    lastName: "Record"
+                }).then(function(chicken) {
+                    // res.send(chicken);
+                })
+            }
+        })
+    }
+
+    function checkOrganization() {
+        db.Organization.count().then(function(c) {
+            console.log("There are " + c + " organizations!")
+            if (c == 0) {
+                db.Organization.create({
+                    name: "Golden State Warriors"
+                }).then(function(chicken) {
+                    // res.send(chicken);
+                })
+            }
+        })
+    }
+
+    function checkSubscription() {
+        db.Subscription.count().then(function(c) {
+            console.log("There are " + c + " subscriptions!")
+            if (c == 0) {
+                db.Subscription.create({
+                    UserId: 1,
+                    OrganizationId: 1,
+                    name: "My Summer Subscription"
+                }).then(function(chicken) {
+                    // res.send(chicken);
+                })
+            }
+        })
+    }
+
+    function checkTicket() {
+        db.Ticket.count().then(function(c) {
+            console.log("There are " + c + " tickets!")
+            if (c == 0) {
+                var d = new Date().toISOString();
+                db.Ticket.create({
+                    UserId: 1,
+                    OrganizationId: 1,
+                    SubscriptionId: 1,
+                    date: d,
+                    seatSec: 225,
+                    seatRow: "I",
+                    seatNum: 16,
+                    status: "locked",
+                    subscription: true
+                }).then(function(chicken) {
+                    // res.send(chicken);
+                })
+            }
+        })
+    }
+}
