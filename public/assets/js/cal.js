0790079345c71b977d4c4e2816e0a36534e7f8d8
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-03-12 21:49:08 -0700

    housekeeping

--- public/assets/js/cal.js
+++ public/assets/js/cal.js
@@ -1,86 +1,86 @@
 
 $(document).ready(function() {
 
-    $(".dropdown-item").on("click", function() {
-        event.preventDefault();
-        var subscription = $(this).data("value");
-        console.log(subscription);
-        getEventInfo(subscription);
-    })
+    // $(".dropdown-item").on("click", function() {
+    //     event.preventDefault();
+    //     var subscription = $(this).data("value");
+    //     console.log(subscription);
+    //     getEventInfo(subscription);
+    // })
 
-    $("#seat-submit").on("click", function() {
-        event.preventDefault();
-        var seatSec = $("#seat-section").val();
-        var seatRow = $("#seat-row").val();
-        var seatNum = $("#seat-number").val();
-        console.log(seatSec, seatRow, seatNum);
-    })
+    // $("#seat-submit").on("click", function() {
+    //     event.preventDefault();
+    //     var seatSec = $("#seat-section").val();
+    //     var seatRow = $("#seat-row").val();
+    //     var seatNum = $("#seat-number").val();
+    //     console.log(seatSec, seatRow, seatNum);
+    // })
 
-    function getEventInfo(subscription) {
-        // https://api.sportradar.us/nba/{access_level}/{version}/{language_code}/games/{season_year}/{nba_season}/schedule.{format}?api_key={your_api_key}
-        var nbaUrl = "https://api.sportradar.us/nba/trial/v4/en/games/2017/reg/schedule.json?api_key=qt53q4h4nux3nzv8xzyfwusw";
-        // + keys.nbakey;
-        var mlbUrl = "https://api.sportradar.us/mlb/trial/v6.5/en/games/2018/reg/schedule.json?api_key=mkapyy5p9ra9yz5y3km4v8tb";
-        // + keys.mlbkey;
+    // function getEventInfo(subscription) {
+    //     // https://api.sportradar.us/nba/{access_level}/{version}/{language_code}/games/{season_year}/{nba_season}/schedule.{format}?api_key={your_api_key}
+    //     var nbaUrl = "https://api.sportradar.us/nba/trial/v4/en/games/2017/reg/schedule.json?api_key=qt53q4h4nux3nzv8xzyfwusw";
+    //     // + keys.nbakey;
+    //     var mlbUrl = "https://api.sportradar.us/mlb/trial/v6.5/en/games/2018/reg/schedule.json?api_key=mkapyy5p9ra9yz5y3km4v8tb";
+    //     // + keys.mlbkey;
 
-        switch (subscription) {
-            case "GSW":
-                $.ajax({
-                    url: nbaUrl,
-                    method: "GET"
-                }).done(function(response) {
-                    var events = [];
-                    for (var i = 0; i < response.games.length; i++) {
-                        //console.log(response)
+    //     switch (subscription) {
+    //         case "GSW":
+    //             $.ajax({
+    //                 url: nbaUrl,
+    //                 method: "GET"
+    //             }).done(function(response) {
+    //                 var events = [];
+    //                 for (var i = 0; i < response.games.length; i++) {
+    //                     //console.log(response)
 
-                        if (response.games[i].home.alias === "GSW") {
-                            var event = {};
-                            console.log(response.games[i].away.name);
-                            console.log(response.games[i].scheduled);
+    //                     if (response.games[i].home.alias === "GSW") {
+    //                         var event = {};
+    //                         console.log(response.games[i].away.name);
+    //                         console.log(response.games[i].scheduled);
 
-                            var away = response.games[i].away.name
-                            event["title"] = "Golden State Warriors vs " + away;
-                            event["start"] = response.games[i].scheduled;
-                            events.push(event);
-                        }
+    //                         var away = response.games[i].away.name
+    //                         event["title"] = "Golden State Warriors vs " + away;
+    //                         event["start"] = response.games[i].scheduled;
+    //                         events.push(event);
+    //                     }
 
-                    }
-                    console.log(events);
-                    renderCalendar(events);
-                });
-                break;
+    //                 }
+    //                 console.log(events);
+    //                 renderCalendar(events);
+    //             });
+    //             break;
 
-            case "SF":
-                $.ajax({
-                    url: mlbUrl,
-                    method: "GET"
-                }).done(function(response) {
-                    var events = [];
-                    for (var i = 0; i < response.games.length; i++) {
-                        //console.log(response)
-                        if (response.games[i].home.abbr === "SF") {
-                            var event = {};
-                            console.log(response.games[i].away.name);
-                            var away = response.games[i].away.name;
-                            event["title"] = "Giants vs " + away;
-                            console.log(response.games[i].scheduled);
-                            event["start"] = response.games[i].scheduled;
-                            events.push(event);
-                        }
-                    }
-                    console.log(events);
-                    renderCalendar(events);
-                });
-                break;
-        }
-    }
+    //         case "SF":
+    //             $.ajax({
+    //                 url: mlbUrl,
+    //                 method: "GET"
+    //             }).done(function(response) {
+    //                 var events = [];
+    //                 for (var i = 0; i < response.games.length; i++) {
+    //                     //console.log(response)
+    //                     if (response.games[i].home.abbr === "SF") {
+    //                         var event = {};
+    //                         console.log(response.games[i].away.name);
+    //                         var away = response.games[i].away.name;
+    //                         event["title"] = "Giants vs " + away;
+    //                         console.log(response.games[i].scheduled);
+    //                         event["start"] = response.games[i].scheduled;
+    //                         events.push(event);
+    //                     }
+    //                 }
+    //                 console.log(events);
+    //                 renderCalendar(events);
+    //             });
+    //             break;
+    //     }
+    // }
 
 
-    $.get("/api/schedule/warriors", function(data) {
+    // $.get("/api/schedule/warriors", function(data) {
 
-        console.log(data)
+    //     console.log(data)
 
-    })
+    // })
 
 renderCalendar()
     function renderCalendar(events) {
@@ -122,6 +122,22 @@ $(document).on("submit", ".req-form", function(event) {
     console.log("mod-sub")
     console.log($("#org").val().trim(), $("#date").val().trim(), $("#num-tix").val().trim())
 })
+
+function returnAllTix() {
+     $.get("/api/tickets", function(data) {
+      
+      for (var i = 0; i < data.length ; i++) {
+        if (data[i].UserId == userID) {
+            console.log("cal-tix!!!!!!", data[i])
+        }
+      }
+      console.log("literally all the tickets", data)
+      console.log("only some :(" , data[7].UserId, userID)
+      })
+     
+}
+returnAllTix()
+
 
 var cookie = document.cookie.split(";");
 console.log("cookie", cookie)
