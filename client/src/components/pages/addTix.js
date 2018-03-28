b10d4b97ffe44adffc9b2f1d805a075f48a19331
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-03-27 20:49:06 -0700

    prepare for merge

--- client/src/components/pages/addTix.js
+++ client/src/components/pages/addTix.js
@@ -1,18 +1,67 @@
-import React from "react";
-import API from "../../utils/API";
-import { Link } from "react-router-dom";
+// import React from "react";
+// import API from "../../utils/API";
+// import { Link } from "react-router-dom";
+// import { Input, FormBtn } from "../../components/Form";
 
-const addTix = () => (
-  <div className="navTabs">
-    <h1>addTix Page</h1>
-    <p>
-      Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
-      ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
-      quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-      imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-      diam, sit amet facilisis lectus blandit at.
-    </p>
-  </div>
-);
+// class addTix extends React.Component {
+//   handleInputChange = event => {
+//     const { name, value } = event.target;
+//     this.setState({
+//       [name]: value
+//     });
+//   };
 
-export default addTix;
+//   handleFormSubmit = event => {
+//     event.preventDefault();
+//     if (this.state.title && this.state.author) {
+//       API.addTickets({
+//         date: this.state.date,
+//         seatSec: this.state.seatSec,
+//         synopsis: this.state.synopsis,
+//         seatRow: this.state.seatRow,
+//         seatNum: this.state.seatNum,
+//         eventTitle: this.state.eventTitle,
+//         status: this.state.status
+        
+//       })
+//         .then(res => this.loadBooks())
+//         .catch(err => console.log(err));
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <div className="navTabs">
+//         <h1>addTix Page</h1>
+//         <form>
+//           <Input
+//             value={this.state.title}
+//             onChange={this.handleInputChange}
+//             name="title"
+//             placeholder="Title (required)"
+//           />
+//           <Input
+//             value={this.state.author}
+//             onChange={this.handleInputChange}
+//             name="author"
+//             placeholder="Author (required)"
+//           />
+//           <TextArea
+//             value={this.state.synopsis}
+//             onChange={this.handleInputChange}
+//             name="synopsis"
+//             placeholder="Synopsis (Optional)"
+//           />
+//           <FormBtn
+//             disabled={!(this.state.author && this.state.title)}
+//             onClick={this.handleFormSubmit}
+//           >
+//             Submit Book
+//               </FormBtn>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+
+// export default addTix;
