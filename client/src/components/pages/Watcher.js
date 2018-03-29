9fb821dfebf5397a659101e8c1d276c5e6ed95f9
Joyce <joycelamsaungan@gmail.com>
2018-03-29 14:55:19 -0700

    fixing watcher

--- client/src/components/pages/Watcher.js
+++ client/src/components/pages/Watcher.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
 import { Link } from "react-router-dom";
-// import { Input, FormBtn } from "../../components/Form";
 
 class Watcher extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      OrganizationId: "",
-      numberOfSeats: "",
+      OrganizationId: "1",
+      numberOfSeats: "1",
       eventDate: "",
       UserId:""
     };
 
     this.handleChange = this.handleChange.bind(this);
+    //this.handleSelect = this.handleSelect.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   };
 
@@ -24,14 +24,6 @@ class Watcher extends Component {
       [name]: value
     });
     console.log("abc", event.target.value)
-    
-    //     this.setState({
-    //   eventDate: event.target.value + " 03:00:00"
-    // })
-
-
-    // console.log('date', this.state.eventDate);
-    
 
     const cookie = document.cookie.split(";");
       console.log("cookie", cookie)
@@ -39,51 +31,47 @@ class Watcher extends Component {
       userID = userID.split("=");
       userID = userID[1];
       console.log("userID:", userID);
-
-
     this.setState({UserId: userID});
-
-
-
   };
 
-  
-    
-  
-
-
-
-
+  // handleSelect = event => {
+  //   console.log("got clicked bru")
+  //   const { name, value } = event.target;
+  //   this.setState({[name]: value})
+  // }
 
   handleSubmit(event) {
     event.preventDefault();
     console.log('org', this.state.OrganizationId, 'seat', this.state.numberOfSeats)
     console.log("submit, date:", this.state.eventDate);
+    var date = this.state.eventDate;
+    this.setState({
+      eventDate:  date + " 03:00:00"
+    })
+    console.log("changed date:", this.state.eventDate);
     console.log("UseId", this.state.UserId);
-
     console.log("dateInt", parseInt(this.state.numberOfSeats));
-    
-  
-    API.addWatcher({
-      // if (this.state.OrganizationId && this.state.eventDate && this.state.numberOfSeats && this.state.UserId) {
+
+    fetch("/api/watchers", {
+      method: 'POST',
+      credentials: "include",
+      mode: "cors",
+      body: JSON.stringify({
         OrganizationId: this.state.OrganizationId,
         eventDate: this.state.eventDate,
         numberOfSeats: this.state.numberOfSeats,
         UserId: this.state.UserId
-      // }
-    }).then(response => 
-      console.log("abcdef")
-      // this.setState({
-      //   OrganizationId: "",
-      //   eventDate: "",
-      //   numberOfSeats: "",
-      //   UserId:""
-      // })
-      ).catch(err => {
-        console.log(err);
+      }),
+      headers: new Headers({
+        'Content-Type': 'application/json'
       })
+    }).then(response  => {
+      console.log(response);
 
-
+      window.location.href = "/";
+    }).catch(err => {
+      console.log(err);
+    })
   }
 
   render() {
@@ -97,7 +85,7 @@ class Watcher extends Component {
               </div>
               <div className="panel-body">
                 <div className="section-content">                 
-                  <div className="select" onSubmit={this.handleSubmit}>
+                  <div className="select">
                     <select className="form-control" value={this.state.OrganizationId} onChange={this.handleChange} name="OrganizationId">
                       <option value="1">Golden State Warriors</option>
                       <option value="2">San Francisco Giants</option>
@@ -115,7 +103,7 @@ class Watcher extends Component {
               </div>
               <div className="panel-body">
                 <div className="section-content">
-                  <div className="select" onSubmit={this.handleSubmit}>
+                  <div className="select">
                     <select className="form-control" value={this.state.numberOfSeats} onChange={this.handleChange} name="numberOfSeats">
                       <option value="1">1</option>
                       <option value="2">2</option>
@@ -136,7 +124,7 @@ class Watcher extends Component {
               </div>
               <div className="panel-body">
                 <div className="section-content">                   
-                  <div className="abc" onSubmit={this.handleSubmit}>
+                  <div className="select-date">
                       <input placeholder="Event Date mm/dd/yyyy" name="eventDate" type="date"value={this.state.eventDate} onChange={this.handleChange}/> 
                     <span className="glyphicon glyphicon-calendar"></span>
                   </div>               
@@ -144,16 +132,10 @@ class Watcher extends Component {
               </div>
             </div>
           </div>
-
-          
-
           <input type="submit" value="Submit" />
         </form>
  
       </div>
-
-
-
     );
   }
 }
