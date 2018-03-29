31bd8c4f7c411d8452aa77b18f1665ef779e38e8
David Booth-Metwally <davidbmetwally@gmail.com>
2018-03-29 14:44:17 -0700

    panel for myRequest

--- client/src/components/pages/MyRequests.js
+++ client/src/components/pages/MyRequests.js
@@ -14,29 +14,22 @@ const MyRequests = () => (
             <br></br>
             <br></br>
             </div>
+    </div>
+    <div className = "row">
             <div className="col-md-6" id="left-ticket">
             <h5 className="card-title c.title">Special title treatment</h5>
             <p className="card-text c.text">With supporting text below as a natural lead-in to additional content.</p>
             </div>
-            <div className="col-md-6" id="right-ticket">
+            <div className="col-md-6 " id="right-ticket">
             <h5 className="card-title c.title">Special title treatment</h5>
             <p className="card-text c.text">With supporting text below as a natural lead-in to additional content.</p>
             </div>
+    </div>
+    <div className = "row">
             <div className="col-md-12">
             <a href="#" className="btn btn-danger" id="decline">Reject</a>
             </div>
-
-        </div>
-        <div className="col-md-5">
-            <h5 className="card-title c.title">Special title treatment</h5>
-            <p className="card-text c.text">With supporting text below as a natural lead-in to additional content.</p>
-        </div>
-        <div className="col-md-12">
-            <a href="#" className="btn btn-primary" className="text-center" id="accept">Go somewhere</a>
-            <a href="#" className="btn btn-primary" id="decline">Go somewhere</a>
-            <br></br>
-            <br></br>
-        </div> 
+    </div>
          <div className="col-md-6">
         </div>
     </div>
@@ -44,9 +37,6 @@ const MyRequests = () => (
   <div className="card-footer text-muted">
     2 days ago
   </div>
-
-</div>
-
 </div>
 );
 
