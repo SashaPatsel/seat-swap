68fcdca6af360895a6a5d69ae8e37f58941be949
David Booth-Metwally <davidbmetwally@gmail.com>
2018-03-29 17:04:54 -0700

    myrequests tab

--- client/src/components/pages/MyRequests.js
+++ client/src/components/pages/MyRequests.js
@@ -2,41 +2,70 @@ import React from "react";
 import "./myRequest.css";
 
 const MyRequests = () => (
-  <div className="navTabs">
-<div className="card text-center">
-  <div className="card-header c.head">
-    Featured
-  </div>
-  <div className="card-body c.body">
-    <div className = "row">
-            <div className="col-md-12">
-            <a href="#" className="btn btn-primary" className="text-center" id="accept">Accept</a>
+  <div className="card-container">
+    <div className="card text-center">
+      <div className="card-header c.head">
+        Featured
+      </div>
+      <div className="card-body c.body">
+        <div className = "row">
+          <div className="col-md-12">
+            <a href="#" className="btn btn-primary text-center"  id="accept">Accept</a>
             <br></br>
             <br></br>
-            </div>
-    </div>
-    <div className = "row">
-            <div className="col-md-6" id="left-ticket">
-            <h5 className="card-title c.title">Special title treatment</h5>
-            <p className="card-text c.text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-            <div className="col-md-6 " id="right-ticket">
-            <h5 className="card-title c.title">Special title treatment</h5>
-            <p className="card-text c.text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-    </div>
-    <div className = "row">
-            <div className="col-md-12">
-            <a href="#" className="btn btn-danger" id="decline">Reject</a>
-            </div>
-    </div>
-         <div className="col-md-6">
+          </div>
         </div>
-    </div>
-  </div>
-  <div className="card-footer text-muted">
-    2 days ago
-  </div>
+        <div className = "row">
+                <div className="col-md-6 requested-ticket">
+                <h5 className="card-title c.title">Oak vs. SF</h5>
+                <p className="card-text c.text">4/3/2018</p>
+                </div>
+                <div className="col-md-6 offered-ticket">
+                <h5 className="card-title c.title">Hamilton</h5>
+                <p className="card-text c.text">5/23/2018</p>
+                </div>
+        </div>
+      </div>
+        <div className = "row card-footer">
+                <div className="col-md-12">
+                <a href="#" className="btn btn-danger" id="decline">Reject</a>
+                </div>
+        </div>
+    </div>  
+
+    
+        <div className="card text-center">
+      <div className="card-header c.head">
+        Featured
+      </div>
+      <div className="card-body c.body">
+        <div className = "row">
+          <div className="col-md-12">
+            <a href="#" className="btn btn-primary text-center"  id="accept">Accept</a>
+            <br></br>
+            <br></br>
+          </div>
+        </div>
+        <div className = "row">
+                <div className="col-md-6 requested-ticket">
+                <h5 className="card-title c.title">Oak vs. SF</h5>
+                <p className="card-text c.text">4/3/2018</p>
+                </div>
+                <div className="col-md-6 offered-ticket">
+                <h5 className="card-title c.title">Hamilton</h5>
+                <p className="card-text c.text">5/23/2018</p>
+                </div>
+        </div>
+      </div>
+        <div className = "row card-footer">
+                <div className="col-md-12">
+                <a href="#" className="btn btn-danger" id="decline">Reject</a>
+                </div>
+        </div>
+    </div>  
+
+
+
 </div>
 );
 
