4773fd4e89e61784d9d3c3c7e3a68dbd489c07fd
Sasha Patsel <sashapatsel@calvisitor-10-105-131-43.calvisitor.1918.berkeley.edu>
2018-04-03 12:24:42 -0700

    times converted, tradeoffers pretty much done

--- client/src/components/MatchOfferListItem.js
+++ client/src/components/MatchOfferListItem.js
@@ -3,7 +3,22 @@ import React from "react";
 const OfferListItem = props => (
   <li className="offer-select" >
     {props.children}
-
+    <table>
+      <tr>
+        <th>Event</th>
+        <th>Date</th>
+        <th>Section</th>
+        <th>Row</th>
+        <th>Number</th>
+      </tr>
+      <tr>
+        <td>{props.eventTitle}</td>
+        <td>{props.date}</td>
+        <td>{props.seatSec}</td>
+        <td>{props.seatRow}</td>
+        <td>{props.seatNum}</td>
+      </tr>
+    </table>
   </li>
 );
 
