afdc2c240e27a3db0f2bf20ae4c2533168aef415
Joyce <joycelamsaungan@gmail.com>
2018-04-03 12:32:13 -0700

    modifying styling to name

--- client/src/components/Banner2/Banner2.js
+++ client/src/components/Banner2/Banner2.js
@@ -4,8 +4,8 @@ import "./Banner2.css";
 const Banner2 = props => (
 	<div id="banner2">
 		<div className="container-fluid banner2">
-			<h1>Seat Swap</h1>
-			<h2>Swap Tickets Anytime Anywhere!</h2>
+			<h1 className="banner-text">Seat Swap</h1>
+			<h2 className="banner-text">Swap Tickets Anytime Anywhere!</h2>
 		</div>
 	</div>
 );
