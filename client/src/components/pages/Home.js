d6a37a48a1d017373d169500f8cf1aa9fcf966d4
Sasha Patsel <sashapatsel@calvisitor-10-105-131-43.calvisitor.1918.berkeley.edu>
2018-04-03 10:56:09 -0700

    fixed navpill bugs

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -25,10 +25,10 @@ class Home extends React.Component {
       <div className="ss-wrapper">
         <Nav />
         <div className="row">
-          <div className="col-md-7">
+          <div className="col-md-6">
             <FullCal />
           </div>
-          <div className="col-md-5">
+          <div className="col-md-6">
             <div className="rightNav">
               <Navpills />
             </div>
