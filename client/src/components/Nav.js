86f7fb163c05f006762c1f58a0c759718c5df33f
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-04-12 10:47:53 -0700

    prepare for branch

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,21 +2,30 @@ import React, { Component } from "react";
 import { Button, Icon } from 'semantic-ui-react';
 
 const Nav = () => (
-<nav className="navbar navbar-default ss-nav">
-    <div className="container-fluid">
-        <div className="navbar-header">
-            <a className="navbar-brand" href="#">
-                <img className="logo" src="https://www.shareicon.net/download/2015/10/05/651639_cinema_512x512.png" />
-            </a>
-            <a href="#">
-            <h2 className="nav-brand">Seat Swap</h2>
-            </a>
+    <nav className="navbar navbar-default ss-nav">
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-1">
+                <div className="navbar-header">
+                    <a className="navbar-brand" href="#">
+                        <img className="logo" src="https://www.shareicon.net/download/2015/10/05/651639_cinema_512x512.png" />
+                    </a>
+                </div>
+                </div>
+                <div className="col-md-4">
+                <a href="#">
+                    <h2 className="nav-brand">Seat Swap</h2>
+                </a>
+                </div>
+                <div className="col-md-5"></div>
+                <div className="col-md-2">
+                <a href="/logout" className="signOut">Sign Out</a>
+                </div>
+            </div>
         </div>
-        <a href="/logout" className="signOut">Sign Out</a>
-    </div>
-</nav>
-    
-  
+    </nav>
+
+
 );
 
 export default Nav;
