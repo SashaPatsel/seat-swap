1c8b6f3c82d1f1ce5de05b68c15e1690cfb8b7d7
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-04-24 18:40:12 -0700

    animated background

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -4,53 +4,28 @@ import SignUp from "../SignUp";
 // import Banner from "../Banner";
 // import Banner2 from "../Banner2";
 import "./Landing.css";
-import JoinUs from "../JoinUs"
-import Modal from 'react-responsive-modal';
+import JoinUs from "../JoinUs";
+import { findDOMNode } from "react-dom";
+// import Modal from "../Modal";
+// import $ from "jquery";
 // import Nav from "../Nav"
 //fadein/fadeout components for background?
 
-const styles = {
-	giants: {
-		background: 'url(https://images.pexels.com/photos/269948/pexels-photo-269948.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350) no-repeat fixed',
-		backgroundSize: "cover",
-		height: "100%",
-		width: "100%", 
-		position: "absolute"
-
-	},
-	symphony: {
-		background: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCgzmyFV3jKAMnpvjPQsUgQ4z40WAKArjofDHh4QzquTE-FmPrdQ) no-repeat fixed',
-		backgroundSize: "cover",
-		height: "100%",
-		width: "100%",
-		position: "absolute"
-	},
-	warriors: {
-		background: 'url(https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-s3.si.com%2Fs3fs-public%2Fstyles%2Fmarquee_large_2x%2Fpublic%2F2015%2F06%2F05%2Foracle-arena-warriors.jpg%3Fitok%3DD4IcX6-B&w=700&q=85) no-repeat fixed',
-		backgroundSize: "cover",
-		height: "100%",
-		width: "100%",
-		position: "absolute"
-	},
-	mainContainer: {
-		textAlign: "center",
-		color: "white",
-	}
-}
 
 class Landing extends Component {
 
 	state = {
 		changingBackground: "giants",
 		open: false,
-		showCloseIcon: true
+		showCloseIcon: true,
+		
 	}
-
-
 
 	componentDidMount() {
 		const landing = this
-		this.changeBackground()
+		// this.changeBackground()
+		// $("#initModal").modal("show")
+		// $("#myModal").modal();
 	}
 
 	changeBackground() {
@@ -66,7 +41,7 @@ class Landing extends Component {
 			}
 
 			landing.newBackground(backgrounds[bGroundIndex])
-		}, 4000)    
+		}, 4000)
 
 	}
 
@@ -77,27 +52,32 @@ class Landing extends Component {
 	}
 
 
-  onOpenModal = () => {
-    this.setState({ open: true });
-  };
+	onOpenModal = () => {
+		this.setState({ open: true });
+	};
 
-  onCloseModal = () => {
-    this.setState({ open: false });
-  };
+	onCloseModal = () => {
+		this.setState({ open: false });
+	};
 	render() {
 		const { open } = this.state;
+
 		return (
 			<div id="landing-container" className={this.state.changingBackground}>
 				<div className="landing-content">
 					<h1 className="roboto main-title">Seat Swap</h1>
-					<h2 className="roboto">The Bay Area's Ticket Exchange</h2>
+					<h2 className="roboto main-sub">The Bay Area's Ticket Exchange</h2>
 					<JoinUs
 						modal={this.onOpenModal}
 					/>
-
-
 				</div>
-        <Modal open={open} onClose={this.onCloseModal} little
+
+	
+				
+				{/* <Modal 
+				
+				/> */}
+				{/* <Modal open={open} onClose={this.onCloseModal} little
 				showCloseIcon = {this.state.showCloseIcon}
 				>
 				<div className="row" id="login">
@@ -108,9 +88,10 @@ class Landing extends Component {
 							<SignIn />
 						</div>
 					</div>
-        </Modal>
+        </Modal> */}
+			<div class="chicken"></div>
 
-				
+
 			</div>
 		)
 	}
