268a7e2852db131ebff20a88962c3f16f4a8c8a8
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-07-07 17:13:41 -0700

    compare to integrate

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -3,11 +3,34 @@ import SignIn from "../components/SignIn";
 import SignUp from "../components/SignUp";
 
 class Landing extends Component {
+state = {
+	scrollY: 0
+}
+	componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+}
+
+componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+}
+
+handleScroll(event) {
+	this.setState({
+		scrollY: window.scrollY
+	})
+	console.log(this.state.scrollY)
+
+}
+doScroll() {
+	console.log("yo")
+}
 	render() {
 		return (
 			<div> 
 				<SignUp/>
 				<SignIn/>
+				<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+					
 			</div>
 		)
 	}
