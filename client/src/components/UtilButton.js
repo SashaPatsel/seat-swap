5807e6c431be81ae37e0dfa80159440df00b6447
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-09-02 11:06:21 -0700

    add tix and subs buttons finished

--- client/src/components/UtilButton.js
+++ client/src/components/UtilButton.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 const UtilButton = props => (
-  <button className="util-button">
-    {props.content}
-   </button> 
+  <div className="util-button">
+    <a className="util-button__icon-container" href={props.destination}>
+      <i className={`fas util-button__icon ${props.icon}`}></i>
+      </a>
+      <label className="util-button__icon-label">{props.label}</label>
+    
+  </div>
+
+
 )
 
 export default UtilButton;
--- client/src/pages/YourTickets.js
+++ client/src/pages/YourTickets.js
@@ -1,6 +1,6 @@
 import React from "react";
 import API from "./../utils/API";
-import Calendar from "../components/Calendar"
+import UtilButton from "../components/UtilButton"
 import FullCal from "../components/Calendar";
 import FullCalendar from 'fullcalendar-reactwrapper';
 import 'fullcalendar-reactwrapper/dist/css/fullcalendar.min.css'
@@ -17,12 +17,18 @@ class YourTickets extends React.Component {
   render() {
 
     return (
-
-      <div className="container">
-        <FullCal />
-
+      <div>
+      <div className="your-tickets__new-tix">
+      <UtilButton destination="#" icon="fa-plus" label="Add tickets"/>
       </div>
+      <div className="your-tickets__new-sub">
+      <UtilButton destination="#" icon="fa-plus" label="Add subscriptions"/>
+      </div>
+        <div className="container">
+          <FullCal />
 
+        </div>
+      </div>
     )
   }
 }
