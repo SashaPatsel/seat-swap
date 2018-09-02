a6abb744b7d61ed0443196fd50964cf606c21b9f
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-09-02 09:58:48 -0700

    basic styling for card component

---
+++ client/src/components/Card.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Card = props => (
+  <div className="ss-card">
+    {props.children}
+  </div>
+)
+
+export default Card
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -6,6 +6,7 @@ import Button from "../components/Button";
 import AuthButton from "../components/AuthButton";
 import Input from "../components/Form/Input"
 import Modal from "../components/Modal"
+import Card from "../components/Card"
 
 
 class Landing extends Component {
@@ -28,7 +29,7 @@ class Landing extends Component {
 	render() {
 		return (
 			<div className="landing" id="landing">
-						<div className="intro__parallax"></div>	
+				<div className="intro__parallax"></div>
 				<section className="intro">
 
 					<h1 className="intro__heading--main">Seat Swap</h1>
@@ -40,43 +41,64 @@ class Landing extends Component {
 					<img className="intro__heroes intro__heroes-3" src="https://images.pexels.com/photos/1047442/pexels-photo-1047442.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
 
 					<div className="intro__auth-container--local">
-					<a href="#signUp">
-						<Button type="submit" genre="bttn--white" id="click" onClick={this.handleFormSubmit} text="Start Swapping" />
+						<a href="#signUp">
+							<Button type="submit" genre="bttn--white" id="click" onClick={this.handleFormSubmit} text="Start Swapping" />
 						</a>
 
 						<a href="#signIn"><p className="intro__login-link"> Login</p></a>
 						<div className="intro__oauth">
 
-						<p className="intro__oauth--or">Or sign up with one of our partners:</p>
+							<p className="intro__oauth--or">Or sign up with one of our partners:</p>
 
-						<a href="/auth/facebook">
-						<AuthButton logo="https://1.bp.blogspot.com/-WSdqH3gMHDk/U-xndvvQYrI/AAAAAAAABwg/9OLHbIjiTF8/s1600/facebook%2Blogo%2Bpng%2Btransparent%2Bbackground.png" company="facebook"/>
-						</a>
+							<a href="/auth/facebook">
+								<AuthButton logo="https://1.bp.blogspot.com/-WSdqH3gMHDk/U-xndvvQYrI/AAAAAAAABwg/9OLHbIjiTF8/s1600/facebook%2Blogo%2Bpng%2Btransparent%2Bbackground.png" company="facebook" />
+							</a>
 
-						<a href="/auth/google">
-						<AuthButton logo="http://backgroundcheckall.com/wp-content/uploads/2017/12/google-logo-transparent-background-4.png" company="google"/>
-						</a>
+							<a href="/auth/google">
+								<AuthButton logo="http://backgroundcheckall.com/wp-content/uploads/2017/12/google-logo-transparent-background-4.png" company="google" />
+							</a>
 						</div>
 					</div>
 					<a href="#about" className="intro__scroll-contain">
-        <i className="fa fa-angle-double-down intro__scroll"></i>
-      </a>
+						<i className="fa fa-angle-double-down intro__scroll"></i>
+					</a>
 				</section>
 
 				<Modal modal="signUp" exitDestination="#" h1="Sign Up">
-					<SignUp/>
+					<SignUp />
 				</Modal>
 
 				<Modal modal="signIn" exitDestination="#" h1="Sign In">
-					<SignIn/>
+					<SignIn />
 				</Modal>
 
-	
-			
-			<div className="landing-about" id="about"></div>
+
+
+				<div className="landing-about" id="about">
+					<div className="container">
+						<div className="row">
+							<div class="col-md-3">
+								<Card>
+								</Card>
+							</div>
+							<div class="col-md-3">
+								<Card>
+								</Card>
+							</div>
+							<div class="col-md-3">
+								<Card>
+								</Card>
+							</div>
+							<div class="col-md-3">
+								<Card>
+								</Card>
+							</div>
+						</div>
+					</div>
+				</div>
 
 			</div>
-// Parallax below with an about section. This is super important
+
 
 		)
 	}
